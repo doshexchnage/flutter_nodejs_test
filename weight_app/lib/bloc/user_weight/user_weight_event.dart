20896799e018b15b71fe7904833cf7e640919ce2
Sandile Khendle <skhendle@gmail.com>
2022-01-05 12:41:11 +0200

    setup user weight bloc

---
+++ weight_app/lib/bloc/user_weight/user_weight_event.dart
@@ -0,0 +1,8 @@
+part of 'user_weight_bloc.dart';
+
+abstract class UserWeightEvent extends Equatable {
+  const UserWeightEvent();
+
+  @override
+  List<Object> get props => [];
+}
---
+++ weight_app/lib/bloc/user_weight/user_weight_state.dart
@@ -0,0 +1,10 @@
+part of 'user_weight_bloc.dart';
+
+abstract class UserWeightState extends Equatable {
+  const UserWeightState();
+  
+  @override
+  List<Object> get props => [];
+}
+
+class UserWeightInitial extends UserWeightState {}
